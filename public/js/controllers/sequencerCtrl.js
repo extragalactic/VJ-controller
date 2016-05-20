d59a5d41fad6ddfa263b34f15115b9df3fc94f6d
Matt <matt.elf@gmail.com>
2016-05-20 02:52:15 -0700

    general improvements

--- public/js/controllers/sequencerCtrl.js
+++ public/js/controllers/sequencerCtrl.js
@@ -8,7 +8,7 @@ angular.module('myApp').controller('SequencerController', ['$scope', '$window', 
   function initControls () {
     console.log('init sequencer');
 
-    // set colors
+    // set colors (.. migrate to a ColorPalette Service)
     var firstColorLight = "#ba6fe8";
     var firstColorMedium = "#7D3CA3";
     var firstColorDark = "#3F1756";
@@ -22,6 +22,9 @@ angular.module('myApp').controller('SequencerController', ['$scope', '$window', 
     var firstColorMediumComp = "#62a33c";
     var firstColorDarkComp = "#2e4e1c";
 
+    // for greying out a disabled matrix...
+    var colorDisabledDark = "#333333";
+
     $window.nx.colorize(firstColorLight);
     $window.nx.colorize("border", firstColorMediumComp);
     $window.nx.colorize("fill", firstColorDark);
@@ -31,7 +34,7 @@ angular.module('myApp').controller('SequencerController', ['$scope', '$window', 
     var bSequencerActive = false;
     var sequencerBPM = 180;
     var clipBankNum = [0,0,0]; // values: 0-2
-
+    var tempoMultiplier = 1;
 
     /*
      The automation assumes the following Resolume layer structure:
@@ -71,10 +74,11 @@ angular.module('myApp').controller('SequencerController', ['$scope', '$window', 
         socket.emit('messageOSC', msgOSC, 1);
       }
     });
-
+/*
     // BPM multiplier slider
     $window.nx.add("slider", {name: "tempoMultiplierSlider", parent:"rightControls"});
     widget = $window.nx.widgets.tempoMultiplierSlider;
+    widget.val = 0;
     widget.init();
     widget.on('*', function(data) {
       var tempoMultiplier = 1 + (data.value * 3);
@@ -82,7 +86,7 @@ angular.module('myApp').controller('SequencerController', ['$scope', '$window', 
       $window.nx.widgets.matrixLayer2.sequence(sequencerBPM*tempoMultiplier);
       $window.nx.widgets.matrixLayer3.sequence(sequencerBPM*tempoMultiplier);
     });
-
+*/
     // BPM control widget
     $window.nx.add("number", {name: "sequencerBPM", parent:"rightControls"});
     widget = $window.nx.widgets.sequencerBPM;
@@ -92,20 +96,44 @@ angular.module('myApp').controller('SequencerController', ['$scope', '$window', 
     widget.init();
     widget.on('*', function(data) {
       sequencerBPM = data.value;
-      $window.nx.widgets.matrixLayer1.sequence(sequencerBPM);
-      $window.nx.widgets.matrixLayer2.sequence(sequencerBPM);
-      $window.nx.widgets.matrixLayer3.sequence(sequencerBPM);
+      $window.nx.widgets.matrixLayer1.sequence(sequencerBPM*tempoMultiplier);
+      $window.nx.widgets.matrixLayer2.sequence(sequencerBPM*tempoMultiplier);
+      $window.nx.widgets.matrixLayer3.sequence(sequencerBPM*tempoMultiplier);
+    });
+
+    // BPM multiplier tabs
+    $window.nx.add("tabs", {name: "tempoMultiplierTabs", parent:"rightControls"});
+    widget = $window.nx.widgets.tempoMultiplierTabs;
+  //  widget.val = 0;
+    widget.options = ["x1", "x2", "x4", "x8"];
+    widget.init();
+    widget.on('*', function(data) {
+      console.log(data);
+      var tempos = [1,2,4,8];
+      tempoMultiplier = tempos[data.index];
+      $window.nx.widgets.matrixLayer1.sequence(sequencerBPM*tempoMultiplier);
+      $window.nx.widgets.matrixLayer2.sequence(sequencerBPM*tempoMultiplier);
+      $window.nx.widgets.matrixLayer3.sequence(sequencerBPM*tempoMultiplier);
     });
 
     // re-sync sequencer button
     $window.nx.add("multitouch", {name: "resyncButton", parent:"rightControls"});
     widget = $window.nx.widgets.resyncButton;
-    widget.colors = {accent: firstColorLightComp, fill: "#440000"};
+    widget.colors = {accent: firstColorLightComp, fill: firstColorDarkComp};
     widget.init();
     widget.on('*', function(data) {
       $window.nx.widgets.matrixLayer1.jumpToCol(0);
       $window.nx.widgets.matrixLayer2.jumpToCol(0);
       $window.nx.widgets.matrixLayer3.jumpToCol(0);
+    });
+
+    // create bank selector tabs
+    $window.nx.add("tabs", {name: "bankSelectorTabs", parent:"bottomControls"});
+    widget = $window.nx.widgets.bankSelectorTabs;
+    widget.options = ["1", "2", "3"];
+    widget.init();
+    widget.on('*', function(data) {
+      console.log(data);
     });
 
     // create a 16x4 clip sequencer matrix
@@ -169,5 +197,25 @@ angular.module('myApp').controller('SequencerController', ['$scope', '$window', 
   });
 
   initControls();
+
+/*
+ToDo:
+
+GENERAL:
+ - fix the broken page switching
+
+FADER:
+ - when the fader automater is running, it should also be animating the slider
+ - the fader slider should be a horizontal slider, placed at the bottom beside the on/off button (then widen the multislider to fill the width)
+ - need to initialize the multislider with start values
+ - when selecting a new beat-style tab it should smoothly animate the sliders to their new positions (implement the Robert Penner easing equations)
+
+SEQUENCER:
+ - add enable/disable toggle buttons to turn on/off matrix layers
+ - add a clip transition-velocity slider
+ - make several matrix banks with pre-loaded automation patterns, and order the patterns left-to-right in order of ascending busy-ness (from chill to intense)
+
+
+*/
 
 }]);
